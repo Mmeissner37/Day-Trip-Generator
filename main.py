dfdb5be3a44853b726ce5ae1a9592a1b6837b275
Michaela Meissner <m.p.meissner85@gmail.com>
2022-11-26 10:57:04 -0500

    Added randomization for the other requirements

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# (5 points): As a developer, I want to make at least three commits with descriptive messages. 
 
 # (5 points): As a user, I want a destination to be randomly selected for my day trip. 
 # (5 points): As a user, I want a restaurant to be randomly selected for my day trip. 
@@ -19,7 +18,7 @@ print(greeting)
 destinations = ['Estes Park, CO', 'Orlando, FL', 'San Antonio, TX', 'Portland, ME', 'San Francisco, CA', 'New York City']
 restaurants = ['Grubsteak Restaurant', 'Santiagos Bodega Tapas Bar', '18 Oaks Steakhouse', 'Fore Street Restaurant', 'Sotto Mare Seafood', 'Jekyll and Hyde Club']
 mode_of_transportations = ['rental car', 'airplane', 'train', 'horseback']
-form_of_entertainments = ['Hiking', 'Disney World', 'Ballet', 'Cirque du Soleil', 'Broadway Musical'] 
+form_of_entertainments = ['Go on a hike', 'Go to Disney World', 'Go to a ballet', 'See a Cirque du Soleil show', 'See a Broadway Musical'] 
 
 
 import random
@@ -30,8 +29,8 @@ print("What about the lovely city of", random.choice(destinations), "as a destin
 
 
 
-
-# for restaurant in restaurants:
-#     print("How about (random_.choice(resturant)) as a place to eat? Is that okay? ")
+#print("Great choice! Now how about ", random.choice(restaurants), "as a restaurant?") 
+#print("Excellent! Would you like to travel by", random.choice(mode_of_transportations),"?")
+#print("Fantastic! During your trip what would you like to do? How about", random.choice(form_of_entertainments),"?")
 
 
