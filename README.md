f9fc94a564e1404aa257baf3ea70c3c353cd0ded
Michaela Meissner <m.p.meissner85@gmail.com>
2022-11-27 12:18:43 -0500

    I finished it! All loops are completed, final function is working, and project is complete.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ##Added randomization for destinations 
 ##Close to figuring out user input for selection
 ##Figured out user input and while loop for destination selection
+##Actually figured out how to cycle through the loops and keeping final selection. See line 24 and 26 for beginning
--- main.py
+++ main.py
@@ -13,17 +13,21 @@
 greeting = "Welcome to the Day Trip Generator! Would you like some help deciding on plans for a vacation? Please, allow me to help you. "
 
 print(greeting)
-
+print("")
 
 destinations = ['Estes Park, CO', 'Orlando, FL', 'San Antonio, TX', 'Portland, ME', 'San Francisco, CA', 'New York City']
 restaurants = ['Grubsteak Restaurant', 'Santiagos Bodega Tapas Bar', '18 Oaks Steakhouse', 'Fore Street Restaurant', 'Sotto Mare Seafood', 'Jekyll and Hyde Club']
-mode_of_transportations = ['Rental car', 'Airplane', 'Train', 'Horseback']
-form_of_entertainments = ['Go on a hike', 'Go to Disney World', 'Go to a ballet', 'See a Cirque du Soleil show', 'See a Broadway Musical'] 
+mode_of_transportations = ['Rental car', 'Horseback', 'Airplane', 'Train', 'Helicopter']
+form_of_entertainments = ['See a Cirque du Soleil show', 'Go to a play', 'Go to a ballet', 'Go to Disney World', 'Go to a musical'] 
 
 import random
-i = 0
+d = 0
+r = 0
+t = 0 
+e = 0
 
-print("What about the lovely city of", destinations[0], "as a destination? Is that okay? Yes or No")
+#Begin destinations
+print("What about the lovely city of", destinations[d], "as a destination? Is that okay? Yes or No")
 
 valid_response = False
 while valid_response == False: 
@@ -32,48 +36,99 @@ while valid_response == False:
         print("Great choice!")
         valid_response = True 
     elif user_input == "No":
-        print("Oh, my apologies. How about", destinations[i],"?")
-        i = i + 1
+        d = d + 1
+        print("Oh, my apologies. Let's chose something else. How about", destinations[d],"?")
         valid_response = False
     else:
         print("I'm sorry. Please say Yes or No.")
         valid_response = False 
-final_dest = destinations[i - 1]
+final_dest = destinations[d]
 
-print(final_dest, "is a great choice!")
-
+print(final_dest, "is a wonderful city to explore!")
+print("")
 #Next is restaurant
-
-print("Next, let's chose a restaurant. How about", restaurants[0],"?")
+print("Next, let's chose a delicous restaurant. How about", restaurants[0],"?")
 
 valid_response = False
 while valid_response == False: 
     user_input = input("")
     if user_input == "Yes":
-        print("Excellent!")
+        print("Yum! That sounds amazing!")
         valid_response = True 
     elif user_input == "No":
-        print("Oh, my apologies. How about", restaurants[i],"?")
-        i = i + 1
+        r = r + 1
+        print("Oh, my apologies. Let's try another one. How about", restaurants[r],"?")
         valid_response = False 
     else:
         print("I'm sorry. Please say Yes or No.")
         valid_response = False 
-final_rest = restaurants[i - 1]
+final_rest = restaurants[r]
 print(final_rest, "is a fantastic decision!")
+print("")
+
+#Modes of transportation
+print("Next, we'll decide on a method of transportation. How about", mode_of_transportations[0],"? Isn't that exciting? Yes or No")
+
+valid_response = False
+while valid_response == False: 
+    user_input = input("")
+    if user_input == "Yes":
+        print("Great choice!")
+        valid_response = True 
+    elif user_input == "No":
+        t = t + 1
+        print("Oh, my apologies. Let me try again. How about", mode_of_transportations[t],"?")
+        valid_response = False
+    else:
+        print("I'm sorry. Please say Yes or No.")
+        valid_response = False 
+final_mode_of_trans = mode_of_transportations[t]
+
+print(final_mode_of_trans, "sounds exciting!")
+print("")
+
+#Entertainment 
+print("Finally, during your trip what would you like to do? How about", form_of_entertainments[0],"? Doesn't that sound lovely? Yes or No")
+
+valid_response = False
+while valid_response == False: 
+    user_input = input("")
+    if user_input == "Yes":
+        print("I'm so excited for you!")
+        valid_response = True 
+    elif user_input == "No":
+        e = e + 1
+        print("Oh, my apologies. Let me try again. How about", form_of_entertainments[e],"?")
+        valid_response = False
+    else:
+        print("I'm sorry. Please say Yes or No.")
+        valid_response = False 
+final_entertainment = form_of_entertainments[e]
+
+print(final_entertainment, "sounds exciting!")
+print("")
+
+#Let's wrap this up
+print("All right! Let's confirm everything:")
+def run():
+    print(final_dest)
+    print(final_rest)
+    print(final_mode_of_trans)
+    print(final_entertainment) 
+
+run()
+print("Does that all look good? Yes or No")
+print("")
+valid_response = False
+while valid_response == False: 
+    user_input = input("")
+    if user_input == "Yes":
+        print("I'm so excited for you! You'll be visiting the exciting city of", final_dest, "by way of", final_mode_of_trans, "where you'll dine at the award winning", final_rest, ", and finish the evening by", final_entertainment,".")
+    elif user_input == "No":
+        print("I'm sorry. Let's try this again. Please start over.")
+    break
+    
+print("")
 
 
 
-#print("Great choice! Now how about ", final_restaurant, "as a restaurant?") 
-#print("Excellent! Would you like to travel by", final_transportation),"?")
-#print("Fantastic! During your trip what would you like to do? How about", final_entertainment),"?")
-
-
-
-# def run():
-#     print(final_destination)
-#     print(final_restaurant)
-#     print(final_transportation)
-#     print(final_entertainment) 
-
-# run()
